6150e9415ebf85a817879dbb86a2e9879eca95b6
Bryan Nguyen <bnguyen@matterport.com>
2020-04-12 22:11:31 -0700

    Updates

---
+++ precache-manifest.7e96d0379243cae711ba54979631c1b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f46e74021c992dd321550d704042107",
+    "url": "/bryan-web-resume/index.html"
+  },
+  {
+    "revision": "0437334196069983af68",
+    "url": "/bryan-web-resume/static/css/main.0d45c96f.chunk.css"
+  },
+  {
+    "revision": "4a1893b856278e1ddff5",
+    "url": "/bryan-web-resume/static/js/2.9d085c32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bryan-web-resume/static/js/2.9d085c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0437334196069983af68",
+    "url": "/bryan-web-resume/static/js/main.2005d161.chunk.js"
+  },
+  {
+    "revision": "0f9b37ddcc094a836ce4",
+    "url": "/bryan-web-resume/static/js/runtime-main.bc53d200.js"
+  },
+  {
+    "revision": "3b0c4b15e64db4757eba886504ec37a5",
+    "url": "/bryan-web-resume/static/media/bryan-web.3b0c4b15.svg"
+  },
+  {
+    "revision": "b55d8dd7638171feadca819bdb3064a9",
+    "url": "/bryan-web-resume/static/media/bryan-web.b55d8dd7.ttf"
+  },
+  {
+    "revision": "ce2c4e9d8c0b3ebe6160a2fe5f702ebe",
+    "url": "/bryan-web-resume/static/media/bryan-web.ce2c4e9d.woff"
+  }
+]);
