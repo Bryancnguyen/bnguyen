fe3d03ac84b9e65fad3a0b0ba98b45f64297388d
Bryan Nguyen <bnguyen@matterport.com>
2020-04-21 14:30:28 -0700

    more hooks

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -1,61 +1,58 @@
-import React, { RefObject } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './modal.scss';
 
-class Modal extends React.Component<ModalProps, ModalState> {
-  private contentRef: React.Ref<HTMLDivElement> | null = null;
+export const Modal: React.FC<ModalProps> = ({ onClose, children, showScroll }) => {
+  const [iconScrollTop, setIconScrollTop] = useState(0);
+  const [scrolled, setScrolled] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
 
-  constructor(props: ModalProps) {
-    super(props);
-    this.state = {
-      iconDownTop: 0,
-      scrolled: false,
+  useEffect(() => {
+    if (contentRef && contentRef.current) {
+      const currentHeight = contentRef.current.clientHeight;
+      setIconScrollTop(currentHeight ? currentHeight - 40 : 0);
+    }
+
+    const handleESC = (event: KeyboardEvent) => {
+      if (event.keyCode === 27) {
+        onClose();
+      }
     };
-    this.contentRef = React.createRef();
-  }
 
-  private onClose = () => {
-    this.props.onClose();
-  }
-
-  public componentDidMount() {
-    if (this.contentRef) {
-      const ref = this.contentRef as RefObject<HTMLDivElement>;
-      const currentHeight = ref.current?.clientHeight;
-      this.setState({ iconDownTop: currentHeight ? currentHeight - 40 : 0 });
+    window.addEventListener('keydown', handleESC);
+    return () => {
+      window.removeEventListener('keydown', handleESC);
     }
-  }
+  }, [onClose]);
 
-  private onScroll = () => {
-    if (this.contentRef) {
-      const ref = this.contentRef as RefObject<HTMLDivElement>;
-      const scrollTop = ref.current?.scrollTop;
+  function onScroll() {
+    if (contentRef && contentRef.current) {
+      const scrollTop = contentRef.current.scrollTop;
       if (scrollTop && scrollTop > 0) {
-        this.setState({ scrolled: true });
+        setScrolled(true);
       }
-      if (scrollTop === 0 && this.state.scrolled) {
-        this.setState({ scrolled: false });
+      if (scrollTop === 0 && scrolled) {
+        setScrolled(false)
       }
     }
-  }
+  };
 
-  render() {
-    const iconDownStyle = {
-      top: `${this.state.iconDownTop}px`,
-    }
-    return (
-      <div className='modal'>
-        <div className='modal-overlay' onClick={this.onClose} />
-        <div className='modal-container' ref={this.contentRef} onScroll={this.onScroll}>
-          <span className='bryan icon-close' onClick={this.onClose} />
-          <div className='content'>
-            {this.props.children}
-            {!this.state.scrolled && this.props.showScroll &&
-              <span className='bryan icon-down' style={iconDownStyle} />
-            }
-          </div>
-        </div>
-      </div>);
+  const iconDownStyle = {
+    top: `${iconScrollTop}px`,
   }
+  return (
+    <div className='modal'>
+      <div className='modal-overlay' onClick={onClose} />
+      <div className='modal-container' ref={contentRef} onScroll={onScroll}>
+        <span className='bryan icon-close' onClick={() => onClose()} />
+        <div className='content'>
+          {children}
+          {!scrolled && showScroll &&
+            <span className='bryan icon-down' style={iconDownStyle} />
+          }
+        </div>
+      </div>
+    </div>
+  )
 }
 
 interface ModalProps {
